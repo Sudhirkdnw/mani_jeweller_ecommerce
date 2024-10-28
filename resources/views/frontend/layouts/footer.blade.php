8f232af254284e5df0f678390fc5aca0c7202edb
Sudhir Kumar <sudhirkdnw@gmail.com>
2024-10-28 23:38:58 +0530

    initial commit

---
+++ resources/views/frontend/layouts/footer.blade.php
@@ -0,0 +1,45 @@
+
+  <!-- JS
+============================================ -->
+
+    <!-- jQuery JS -->
+    <script src="{{asset ('frontend/assets/js/vendor/jquery-3.6.0.min.js')}}"></script>
+    <script src="{{asset ('frontend/assets/js/vendor/jquery-migrate-3.3.2.min.js')}}"></script>
+    <!-- Modernizer JS -->
+    <script src="{{asset ('frontend/assets/js/vendor/modernizr-3.11.2.min.js')}}"></script>
+    <!-- Bootstrap JS -->
+    <script src="{{asset ('frontend/assets/js/vendor/bootstrap.bundle.min.js')}}"></script>
+
+    <!-- Slick Slider JS -->
+    <script src="{{asset ('frontend/assets/js/plugins/slick.min.js')}}"></script>
+    <!-- Countdown JS -->
+    <script src="{{asset ('frontend/assets/js/plugins/countdown.min.js')}}"></script>
+    <!-- Barrating JS -->
+    <script src="{{asset ('frontend/assets/js/plugins/jquery.barrating.min.js')}}"></script>
+    <!-- Counterup JS -->
+    <script src="{{asset ('frontend/assets/js/plugins/jquery.counterup.min.js')}}"></script>
+    <!-- Waypoints -->
+    <script src="{{asset ('frontend/assets/js/plugins/waypoints.min.js')}}"></script>
+    <!-- Nice Select JS -->
+    <script src="{{asset ('frontend/assets/js/plugins/jquery.nice-select.min.js')}}"></script>
+    <!-- Sticky Sidebar JS -->
+    <script src="{{asset ('frontend/assets/js/plugins/jquery.sticky-sidebar.js')}}"></script>
+    <!-- Jquery-ui JS -->
+    <script src="{{asset ('frontend/assets/js/plugins/jquery-ui.min.js')}}"></script>
+    <!-- Scroll Top JS -->
+    <script src="{{asset ('frontend/assets/js/plugins/scroll-top.min.js')}}"></script>
+    <!-- Theia Sticky Sidebar JS -->
+    <script src="{{asset ('frontend/assets/js/plugins/theia-sticky-sidebar.min.js')}}"></script>
+    <!-- ElevateZoom JS -->
+    <script src="{{asset ('frontend/assets/js/plugins/jquery.elevateZoom-3.0.8.min.js')}}"></script>
+    <!-- Timecircles JS -->
+    <script src="{{asset ('frontend/assets/js/plugins/timecircles.min.js')}}"></script>
+    <!-- Mailchimp Ajax JS -->
+    <script src="{{asset ('frontend/assets/js/plugins/mailchimp-ajax.js')}}"></script>
+
+    <!-- Main JS -->
+    <script src="{{asset ('frontend/assets/js/main.js') }}"></script>
+    <!-- <script src="assets/js/main.min.js"></script> -->
+
+</body>
+</html>
---
+++ resources/views/frontend/layouts/header.blade.php
@@ -0,0 +1,334 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Mani Jewellers</title>
+    <meta name="robots" content="noindex, follow" />
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset ('frontend/assets/images/favicon.ico')}}">
+
+    <!-- CSS
+	============================================ -->
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{asset ('frontend/assets/css/bootstrap.min.css')}}">
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="{{asset ('frontend/assets/css/font-awesome.min.css')}}">
+    <!-- Fontawesome Star -->
+    <link rel="stylesheet" href="{{asset ('frontend/assets/css/fontawesome-stars.css')}}">
+    <!-- Ion Icon -->
+    <link rel="stylesheet" href="{{asset ('frontend/assets/css/ionicons.min.css')}}">
+    <!-- Slick CSS -->
+    <link rel="stylesheet" href="{{asset ('frontend/assets/css/slick.min.css')}}">
+    <!-- Animation -->
+    <link rel="stylesheet" href="{{asset ('frontend/assets/css/animate.min.css')}}">
+    <!-- jQuery Ui -->
+    <link rel="stylesheet" href="{{asset ('frontend/assets/css/jquery-ui.min.css')}}">
+    <!-- Nice Select -->
+    <link rel="stylesheet" href="{{asset ('frontend/assets/css/nice-select.min.css')}}">
+    <!-- Timecircles -->
+    <link rel="stylesheet" href="{{asset ('frontend/assets/css/timecircles.min.css')}}">
+
+    <!-- Main Style CSS -->
+    <link rel="stylesheet" href="{{asset ('frontend/assets/css/style.css')}}">
+    <!-- <link rel="stylesheet" href="assets/css/style.min.css"> -->
+
+</head>
+</head>
+
+<body class="template-color-3">
+
+    <div class="main-wrapper">
+
+        <!-- Begin Hiraola's Header Main Area Three -->
+        <header class="header-main_area header-main_area-3">
+            <div class="header-top_area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="ht-left_area">
+                                <div class="welcome_text">
+                                    <p>Free shipping on all domestic orders with coupon code
+                                        <span>"Earrings0920"</span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="ht-right_area">
+                                <div class="ht-menu">
+                                    <ul>
+                                        <li><a href="javascript:void(0)">Currency<i class="fa fa-chevron-down"></i></a>
+                                            <ul class="ht-dropdown ht-currency">
+                                                <li><a href="javascript:void(0)">€ EUR</a></li>
+                                                <li class="active"><a href="javascript:void(0)">£ Pound Sterling</a></li>
+                                                <li><a href="javascript:void(0)">$ Us Dollar</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="javascript:void(0)">Language <i class="fa fa-chevron-down"></i></a>
+                                            <ul class="ht-dropdown">
+                                                <li class="active"><a href="javascript:void(0)"><img src="assets/images/menu/icon/1.jpg" alt="JB's Language Icon">English</a></li>
+                                                <li><a href="javascript:void(0)"><img src="assets/images/menu/icon/2.jpg" alt="JB's Language Icon">Français</a>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="my-account.html">My Account<i class="fa fa-chevron-down"></i></a>
+                                            <ul class="ht-dropdown ht-my_account">
+                                                <li><a href="javascript:void(0)">Register</a></li>
+                                                <li class="active"><a href="javascript:void(0)">Login</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="header-bottom_area header-bottom_area-2   white--color" style="margin-top: 40px;">
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-2 col-md-4 col-sm-4">
+                        <div class="header-logo">
+                            <a href="#">
+                                <img src="{{asset ('frontend/assets/images/menu/logo/2.png')}}" alt="Hiraola's Header Logo">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-7 d-none d-lg-block position-static">
+                        <div class="main-menu_area">
+                            <nav>
+                                <ul>
+                                    <li class="dropdown-holder"><a href="index.html">Home</a></li>
+                                    <li class="megamenu-holder"><a href="shop-left-sidebar.html">Shop</a> </li>
+                                   
+                                    <li><a href="index.html">Pages</a>
+                                        <ul class="hm-dropdown">
+                                            <li><a href="my-account.html">My Account</a></li>
+                                            <li><a href="login-register.html">Login | Register</a></li>
+                                            <li><a href="wishlist.html">Wishlist</a></li>
+                                            <li><a href="cart.html">Cart</a></li>
+                                            <li><a href="checkout.html">Checkout</a></li>
+                                            <li><a href="compare.html">Compare</a></li>
+                                            <li><a href="faq.html">FAQ</a></li>
+                                            <li><a href="404.html">404 Error</a></li>
+                                            <li><a href="coming-soon_page.html">Comming Soon</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="about-us.html">About Us</a></li>
+                                    <li><a href="contact.html">Contact</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-8 col-sm-8">
+                        <div class="header-right_area">
+                            <ul>
+                                <li>
+                                    <a href="#" class="wishlist-btn">
+                                        <i class="ion-android-favorite-outline"></i>
+                                    </a>
+                                </li>
+                                
+                                <li>
+                                    <a href="#mobileMenu" class="mobile-menu_btn toolbar-btn color--white d-lg-none d-block">
+                                        <i class="ion-navicon"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#miniCart" class="minicart-btn toolbar-btn">
+                                        <i class="ion-bag"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+            
+          
+            <!-- ################################### -->
+            <div class="header-bottom_area header-bottom_area-2 header-sticky stick white--color">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-2 col-md-4 col-sm-4">
+                            <div class="header-logo">
+                                <a href="index.html">
+                                    <img src="{{asset ('frontend/assets/images/menu/logo/2.png')}}" alt="Hiraola's Header Logo">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-lg-7 d-none d-lg-block position-static">
+                            <div class="main-menu_area">
+                                <nav>
+                                    <ul>
+                                        <li class="dropdown-holder"><a href="index.html">Home</a></li>
+                                        <li class="megamenu-holder"><a href="shop-left-sidebar.html">Shop</a>
+                                            
+                                        </li>
+                                       
+                                        <li><a href="index.html">Pages</a>
+                                            <ul class="hm-dropdown">
+                                                <li><a href="my-account.html">My Account</a></li>
+                                                <li><a href="login-register.html">Login | Register</a></li>
+                                                <li><a href="wishlist.html">Wishlist</a></li>
+                                                <li><a href="cart.html">Cart</a></li>
+                                                <li><a href="checkout.html">Checkout</a></li>
+                                                <li><a href="compare.html">Compare</a></li>
+                                                <li><a href="faq.html">FAQ</a></li>
+                                                <li><a href="404.html">404 Error</a></li>
+                                                <li><a href="coming-soon_page.html">Comming Soon</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="about-us.html">About Us</a></li>
+                                        <li><a href="contact.html">Contact</a></li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-8 col-sm-8">
+                            <div class="header-right_area">
+                                <ul>
+                                    <li>
+                                        <a href="#" class="wishlist-btn">
+                                            <i class="ion-android-favorite-outline"></i>
+                                        </a>
+                                    </li>
+                                    
+                                    <li>
+                                        <a href="#mobileMenu" class="mobile-menu_btn toolbar-btn color--white d-lg-none d-block">
+                                            <i class="ion-navicon"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#miniCart" class="minicart-btn toolbar-btn">
+                                            <i class="ion-bag"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- ############################## -->
+           
+            <div class="offcanvas-search_wrapper" id="searchBar">
+                <div class="offcanvas-menu-inner">
+                    <div class="container">
+                        <a href="#" class="btn-close"><i class="ion-android-close"></i></a>
+                        <!-- Begin Offcanvas Search Area -->
+                        <div class="offcanvas-search">
+                            <form action="#" class="hm-searchbox">
+                                <input type="text" placeholder="Search for item...">
+                                <button class="search_btn" type="submit"><i class="ion-ios-search-strong"></i></button>
+                            </form>
+                        </div>
+                        <!-- Offcanvas Search Area End Here -->
+                    </div>
+                </div>
+            </div>
+            <div class="mobile-menu_wrapper" id="mobileMenu">
+                <div class="offcanvas-menu-inner">
+                    <div class="container">
+                        <a href="#" class="btn-close"><i class="ion-android-close"></i></a>
+                        <div class="offcanvas-inner_search">
+                            <form action="#" class="hm-searchbox">
+                                <input type="text" placeholder="Search for item...">
+                                <button class="search_btn" type="submit"><i class="ion-ios-search-strong"></i></button>
+                            </form>
+                        </div>
+                        <nav class="offcanvas-navigation">
+                            <ul class="mobile-menu">
+                                <li class="menu-item-has-children active"><a href="#"><span class="mm-text">Home</span></a>
+                                    
+                                </li>
+                                <li class="menu-item-has-children">
+                                    <a href="#">
+                                        <span class="mm-text">Shop</span>
+                                    </a>
+                                   
+                                </li>
+
+
+                                
+                                <li class="menu-item-has-children">
+                                    <a href="#">
+                                        <span class="mm-text">Pages</span>
+                                    </a>
+                                    <ul class="sub-menu">
+                                        <li>
+                                            <a href="wishlist.html">
+                                                <span class="mm-text">Wishlist</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="cart.html">
+                                                <span class="mm-text">Cart</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="checkout.html">
+                                                <span class="mm-text">Checkout</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="compare.html">
+                                                <span class="mm-text">Compare</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="faq.html">
+                                                <span class="mm-text">FAQ</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="404.html">
+                                                <span class="mm-text">Error 404</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="coming-soon_page.html">
+                                                <span class="mm-text">Comming Soon</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </nav>
+                        <nav class="offcanvas-navigation user-setting_area">
+                            <ul class="mobile-menu">
+                                <li class="menu-item-has-children active">
+                                    <a href="#">
+                                        <span class="mm-text">User
+                                        Setting
+                                    </span>
+                                    </a>
+                                    <ul class="sub-menu">
+                                        <li>
+                                            <a href="my-account.html">
+                                                <span class="mm-text">My Account</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="login-register.html">
+                                                <span class="mm-text">Login | Register</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                               
+                                
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </header>
+        
